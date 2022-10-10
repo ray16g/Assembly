35e7366d0b8711a0ee9edfcf0c455a9db9f4b44a
ray <raymondh2003@yahoo.com>
2022-10-09 19:26:23 -0700

    bit

--- Lab6a/bit_check.asm
+++ Lab6a/bit_check.asm
@@ -10,16 +10,34 @@
 ; why: bit_check.asm
 ; when: 09/10/2022 (DD/MM/YYYY)
 
+extern printstr
 
 section     .text
 
 global      _start
 
-extern 
-
 _start:
+    mov     bl, 00010110b   ; test case
 
-    
+    test     bl, 1110000b    ; Set all bits other than 4,5,6 to 0
+    ; if 4,5,6 bit are not flipped, then bl will be 0
+
+    jz label1   ; no bits are flipped
+
+    ;print
+    mov eax, isSet
+    mov ebx, isSetLen
+    call printstr
+
+    jmp exit    ; jump to exit
+
+    label1:
+
+    ;print
+    mov eax, isNotSet
+    mov ebx, isNotSetLen
+    call printstr
+
 
 exit:  
     mov     ebx, 0      ; return 0 status on exit - 'No Errors'
@@ -29,3 +47,8 @@ exit:
 section     .bss
 
 section     .data
+isSet:  db "Bits 4, 5, or 6 are set.", 0xa
+isSetLen: equ $ - isSet
+
+isNotSet: db "Bits 4, 5, and 6 are not set.", 0xa
+isNotSetLen: equ $ - isNotSet
